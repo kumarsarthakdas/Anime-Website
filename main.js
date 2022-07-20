e0b9e5b7d175ae546cd0c95ecd5bc569e3994f80
Sarthak <sarthak.siddhu.das@gmail.com>
2022-07-20 23:19:51 +0530

    commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var elem = document.querySelector('.summer-2022-anime-carousel');
+var flkty = new Flickity(elem, {
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+    wrapAround: true,
+    freeScroll: true
+});
+
+var elem2 = document.querySelector('.latest-episode-arrivals-carousel');
+var flkty2 = new Flickity(elem2, {
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+    wrapAround: true,
+    freeScroll: true
+});
