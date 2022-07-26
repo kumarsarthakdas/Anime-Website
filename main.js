4cc2733d8109cdc93d070cd82105346c29268e65
Sarthak <sarthak.siddhu.das@gmail.com>
2022-07-27 01:18:47 +0530

    added new_episode_arrivals, popular_anime_trailers

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 var elem = document.querySelector('.summer-2022-anime-carousel');
 var flkty = new Flickity(elem, {
     cellAlign: 'left',
+    groupCells: '70%',
     contain: true,
     pageDots: false,
     wrapAround: true,
     freeScroll: true
 });
 
-var elem2 = document.querySelector('.latest-episode-arrivals-carousel');
+var elem2 = document.querySelector('.new-episode-arrivals-carousel');
 var flkty2 = new Flickity(elem2, {
     cellAlign: 'left',
+    groupCells: '60%',
     contain: true,
     pageDots: false,
     wrapAround: true,
     freeScroll: true
+});
+
+var elem3 = document.querySelector('.popular-anime-trailers-carousel');
+var flkty3 = new Flickity(elem3, {
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+    wrapAround: true,
+    freeScroll: true,
+    lazyLoad: 4
 });
